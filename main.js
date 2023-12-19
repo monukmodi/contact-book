bdc67096e4b47f0704443204d51184f6fe576d11
monukmodi <monukr2012@gmail.com>
2023-12-20 01:50:02 +0530

    Database Connected

--- main.js
+++ main.js
@@ -2,10 +2,16 @@
 require("dotenv").config()
 
 const express = require("express")
+const mongoose = require("mongoose")
 
 const app = express()
 const PORT = process.env.PORT || 4000
 
+// Database Connection
+mongoose.connect(process.env.DB_URI)
+const db = mongoose.connection
+db.on("error", (error) => console.log(error))
+db.once("open", () => console.log("Connected to the Database"))
 app.get("/", (req, res) => {
   res.send("Hello Node JS")
 })
